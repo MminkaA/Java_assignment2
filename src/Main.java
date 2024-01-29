991bd54877bd446b66296d2d75c9fdb2797cb870
Nurislam <154406475+MminkaA@users.noreply.github.com>
2024-01-29 08:34:32 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> people = new ArrayList<>();
-        Student student_1 = new Student("Azat", "Bolatbekov", 3.0);
-        Student student_2 = new Student("Biba", "Koszhanova", 2.5);
-        Employee employee_1 = new Employee("Nurislam", "Meiramov", "Manager", 27045.78);
-        Employee employee_2 = new Employee("Sultan", "Kissamedinov", "Developer", 50000.00);
+        Student student_1 = new Student("Azat", "Bolatbekov", 2.8);
+        Student student_2 = new Student("Biba", "Koszhanova", 3.3);
+        Employee employee_1 = new Employee("Nurislam", "Meiramov", "Manager", 62490.90);
+        Employee employee_2 = new Employee("Sultan", "Kissamedinov", "Developer", 72500.00);
         people.add(employee_1);
         people.add(employee_2);
         people.add(student_1);
